951bcf87a8fc0d2d1dae8ed565b1859d40595f68
alexmariniello <ale.mariniello@gmail.com>
2022-11-25 16:32:21 +0100

    Interfaccia Prima Sprint

---
+++ App.java
@@ -0,0 +1,30 @@
+package hellofx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try {
+            Parent root=FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+            primaryStage.setTitle("Gruppo 14");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+            
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Controller.java
@@ -0,0 +1,100 @@
+
+
+
+
+package hellofx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private AnchorPane ancorPane;
+
+    @FXML
+    private ColorPicker borderColorPicker;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private Label chooseInteriorColorLabel;
+
+    @FXML
+    private Canvas drawingWindow;
+
+    @FXML
+    private Button ellipseButton;
+
+    @FXML
+    private Menu fileMenu;
+
+    @FXML
+    private ColorPicker interiorColorPicker;
+
+    @FXML
+    private MenuItem loadItem;
+
+    @FXML
+    private Button rectangleButton;
+
+    @FXML
+    private MenuItem saveItem;
+
+    @FXML
+    private Button segmentButton;
+
+    @FXML
+    void colorBorder(ActionEvent event) {
+        //function linked to the color picker that color the border of a shape
+    }
+
+    @FXML
+    void colorInterior(ActionEvent event) {
+        //function linked to the color picker that color the interior of a shape
+    }
+
+    @FXML
+    void onLoad(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSave(ActionEvent event) {
+
+    }
+
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        GraphicsContext gc=drawingWindow.getGraphicsContext2D();
+
+        drawingWindow.setOnMouseDragged(e->{
+            double x=e.getX()- 5; //5=size(la size sta nel fill React)/2
+            double y=e.getY()-5;  //stessa cosa di sopra
+
+            gc.setFill(interiorColorPicker.getValue());
+            gc.fillRect(x,y,10,10);
+        });
+        
+    }
+
+}
